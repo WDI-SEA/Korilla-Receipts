b1a21ece9ce88e038f2034e5dee7e544fc415b4c
Ahmed Al-Thawadi <ahmed9althawadi@gmail.com>
2022-11-02 21:29:17 +0300

    task 1 done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Receipt from './components/Receipt/Receipt';
+
 
 const initialState = [
   {
@@ -46,15 +48,171 @@ const initialState = [
   },
 ];
 
+
+
+const updatedState =  {
+  id: 0,
+  person: '',
+  order: {
+    main: '',
+    protein: '',
+    rice: '',
+    sauce: '',
+    toppings: [''],
+    drink: '',
+    cost: 0,
+  },
+  paid: false,
+}
+
+
+
+
+
 function App() {
+
+  const [state, setState] = useState(initialState)
+
+
+  const [newState, setNewState] = useState(updatedState)
+
+
+  const addNewState = (e) => {
+    e.preventDefault()
+
+    // adding the newState or order
+    setState([
+      ...state,
+      newState
+    ])
+
+    // reset the newState saved to empty
+    setNewState(
+      {
+        id: 0,
+        person: '',
+        order: {
+          main: '',
+          protein: '',
+          rice: '',
+          sauce: '',
+          toppings: [''],
+          drink: '',
+          cost: 0,
+        },
+        paid: false,
+      }
+    )
+
+    console.log(state)
+  }
+
+
+  // set the state whenever a user enters words in that input field
+  const handleChange = (e) => {
+  
+    setNewState({
+      ...newState,
+      [e.target.name]: e.target.value
+    })
+    
+    console.log('event target name: ', e.target.name)
+    console.log('event target value: ', e.target.value)
+
+    console.log('Handle change ran')
+  }
+
+
+
   return (
     <>
       <header>
         <h1 className="name">Korilla</h1>
       </header>
-      <main></main>
+      <main>
+        <div className='recipesContainer'>
+          {state.map(p =>
+
+            <Receipt order={p}/>
+          )}
+        </div>
+
+        <div className='newOrder'>
+          <div></div>
+
+        <div>
+        <h3>Add New Order</h3>
+
+        <form onSubmit={addNewState}>
+          <label>
+            <span>Person Name: </span>
+          <input type='text' name='person' value={newState.person} onChange={handleChange}></input>
+          </label>
+          <br />
+            <span>Order: </span>
+            <br />
+
+          <label>
+            <span>Main: </span>
+            <input name='main' value={newState.order.main} onChange={handleChange}></input>
+          </label>
+          <br />
+
+          <label>
+            <span>Protein: </span>
+            <input name='protein' value={newState.order.protein} onChange={handleChange}></input>
+          </label>
+          <br />
+
+          <label>
+            <span>Rice: </span>
+            <input name='rice' value={newState.order.rice} onChange={handleChange}></input>
+          </label>
+          <br />
+
+          <label>
+            <span>Sauce: </span>
+            <input name='sauce' value={newState.order.sauce} onChange={handleChange}></input>
+          </label>
+          <br />
+
+          {/* <label>
+            <span>Toppings: </span>
+            <input name='toppings'></input>
+          </label>
+          <br /> */}
+
+          <label>
+            <span>Drink: </span>
+            <input name='drink' value={newState.order.drink} onChange={handleChange}></input>
+          </label>
+          <br />
+
+          <label>
+            <span>Cost: </span>
+            <input name='cost' value={newState.order.cost} onChange={handleChange}></input>
+          </label>
+          <br />
+
+
+          <br />
+          <button type='submit'>Add Order</button>
+        </form>
+        </div>
+        <div></div>
+        </div>
+      </main>
     </>
   );
 }
+
+/*
+      main: 'Salad Bowl',
+      protein: 'Organic Tofu',
+      rice: 'none',
+      sauce: "K'lla",
+      toppings: ['Blue Potato Salad', 'Pico De Gallo', 'Red Kimchi'],
+      drink: 'Sparkling Blood Orange Soda',
+      cost: 20, */
 
 export default App;
---
+++ src/components/Receipt/Receipt.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Receipt.css'
+// import { UserContext } from '../../App'
+
+function Receipt(props) {
+
+    // const data = useContext(UserContext)
+
+    // console.log('data is:', data)
+
+// console.log(props)
+    // console.log(Object.entries(data[0].order))
+  return (
+    <div className='personDiv'>
+
+
+
+        <h1>{props.order.person}</h1>
+
+        {Object.entries(props.order.order).map(item => 
+            
+            <p style={{fontWeight: 'bold'}}><span style={{color: 'purple'}}>{item[0]}:</span> {item[1]}</p>
+            
+            )}
+
+
+
+
+    </div>
+
+  )
+}
+
+export default Receipt
